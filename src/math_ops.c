ef870bc938cbb5cd3b2053c591ecc01415af5866
SwathantraPulicherla <swathantra.pulicherla@requisimus.com>
2025-09-22 12:05:19 +0000

    non AI

--- src/math_ops.c
+++ src/math_ops.c
@@ -4,5 +4,5 @@ int complex_calc(int a, int b) {
     // Calls add from main.c and compute from utils.c
     int sum = add(a, b);
     int comp = compute(sum);
-    return multiply(comp, 2);  // Also calls multiply from main.c
+    return multiply(comp, 3);  // Also calls multiply from main.c
 }
